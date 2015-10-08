917a2af96dfb258ef79d77345391d7c2814f6b9a
Maarten Flippo <maartenflippo@outlook.com>
2015-10-08 19:14:40 +0000

    Added an auth service, needs implementation

---
+++ angular/app/common/auth.js
@@ -0,0 +1,13 @@
+(function(angular) {
+    'use strict';
+
+    angular
+        .module('app.common')
+        .factory('AuthService', AuthService);
+
+    /* @ngInject */
+    function AuthService() {
+
+    }
+
+})(window.angular);
