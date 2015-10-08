e6f1201b7fc05620ddcb43c5e694aa3feda3c52a
Maarten Flippo <maartenflippo@outlook.com>
2015-10-08 19:32:15 +0000

    Started implementing the auth service, but we need a decision on #20 before I can continue.

--- angular/app/common/auth.js
+++ angular/app/common/auth.js
@@ -6,8 +6,48 @@
         .factory('AuthService', AuthService);
 
     /* @ngInject */
-    function AuthService() {
+    function AuthService($localStorage) {
+        return {
+            login: login,
+            getToken: getToken,
+            isAuthenticated: isAuthenticated,
+            logout: logout
+        };
 
+        /**
+         * Log a user into the application.
+         *
+         * @param {object} user
+         */
+        function login(user) {
+            // Call api for token
+            $localStorage.token = "someToken";
+        }
+
+        /**
+         * Get the JSON Web Token.
+         *
+         * @return {string}
+         */
+        function getToken() {
+            return $localStorage.token;
+        }
+
+        /**
+         * Check wether someone is authenticated.
+         *
+         * @return {boolean}
+         */
+        function isAuthenticated() {
+            return $localStorage.token !== null;
+        }
+
+        /**
+         * Log a user out off the application.
+         */
+        function logout() {
+            delete $localStorage.token;
+        }
     }
 
 })(window.angular);
