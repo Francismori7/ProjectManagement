00e34cc78e0cc3af958937752fce5bafbf6bde0f
Maarten Flippo <maartenflippo@outlook.com>
2015-12-06 16:12:32 +0000

    Updated to work with the api

--- angular/components/projects/projects.controller.js
+++ angular/components/projects/projects.controller.js
@@ -14,6 +14,7 @@
         (function() {
             ProjectsSvc.getAll()
                 .then(function(projects) {
+                    console.log(projects);
                     vm.projects = projects;
                 });
         })();
