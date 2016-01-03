88bc20778296c487960feb05b4bc7ef978ee0f33
Maarten Flippo <maartenflippo@outlook.com>
2016-01-03 13:02:08 +0000

    Some more widget refactoring

--- angular/components/projects/show/general/general.controller.js
+++ angular/components/projects/show/general/general.controller.js
@@ -10,12 +10,20 @@
 		var vm = this;
 
 		vm.getCompletePercentage = getCompletePercentage;
+        vm.getNumAssignedUsers = getNumAssignedUsers;
 
 		function getCompletePercentage(project) {
 			if (!project)
 				return 0;
 			return (project.completedTasks.length || 0) / (project.tasks.length || 0) * 100;
-		}        
+		}
+
+        function getNumAssignedUsers(project) {
+            if (!project)
+                return 0;
+
+            return (project.users) ? project.users.length : 0;
+        }
     }
 
 })(window.angular);
